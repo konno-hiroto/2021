c3b9f66682880fb1487199d4eb4148ee428c46d0
S-JOBI21-111\藤原 <211149@st.yoshida-g.ac.jp>
2021-12-21 15:04:42 +0900

    no message

--- 串鳥アプリ/System/Genre.php
+++ 串鳥アプリ/System/Genre.php
@@ -1,29 +1,47 @@
 <!DOCTYPE html>
 <html>
 <head>   
-    <title>PHP</title>
+    <title>Top画面</title>
     <meta charset="utf-8">
-</head>
-<style>
-    #d{
-            width: calc(100% / 3 - 30px);
-            margin-bottom: 30px;
-            padding: 50px 10px;
-            text-align: center;
-            background-color: coral;
-            font-size: 30px;
+    <style>
+        body{
+            width:1500px;
+        }
+        .items{
+            width: calc(1170px / 4 - 30px);
+            height:100px;
+            margin: 0px 2px;
+            padding: 10px 10px;
+            position:relative;
+            border: 1px solid;
+        }
+        .items p{
+            position:absolute;
+            top:20%;
+            left:20%;
         }
         .contents{
             display: flex;
             flex-wrap: wrap;
-            justify-content: space-around;
-            width: 100%;
-            margin: 30px 0;
-            padding: 120px 0px;
+            width: 78%;
+            margin: 0px 0;
+            padding: 0px 0px;
         }
-        /*body{
-            height:1000px;
-        }*/
+        .menu{
+            width: calc(1170px / 3 - 30px);
+            height:150px;
+            margin: 5px 2px;
+            padding: 10px 10px;
+            position:relative;
+            border: 1px solid;
+        }
+        .contentsA{
+            display: flex;
+            flex-wrap: wrap;
+            width: 78%;
+            margin: 0px 0;
+            padding: 0px 0px;
+        }
         footer{
             position: fixed;
             bottom: 0;
@@ -31,60 +49,90 @@
             text-align: center;
             padding: 30px 0;
         }
-        footer #kaikei,#call,#Amenu,#Bmenu{
-            width: 20%;
+        footer #kaikei,#call,#Amenu,#Bmenu,#login{
+            width: 30%;
             height: 100px; 
             color:blue;
             font-size: 50px;
         }
-        header{
-            position:fixed;
-            width: 100%;
-            text-align: center;
-            height: 112px;
-            margin-top: 0;
-        }
-        header button{
-            width: 15%;
-            height: 80px; 
-            color:blue;
-            font-size: 30px;
-        }
-</style>
+    </style>
+    <script>
+       
+    </script>
+</head>
 <body>
+    <script>
+         
+    </script>
+    <div class="contents">
     <?php
-        try{
-            require_once "connect.php";
-            include 'error.php';
-        
-            $dbconnect = new connect();
-            /*if(isset($_POST["delete"])){
-                //$ya_id="";
-                if(isset($_POST["ya_id"])){
-                    $ya_id=$_POST["ya_id"];
-                    $stmt = $dbconnect->db->prepare('DELETE FROM user1 WHERE id = :id');
-                    $stmt->bindValue(':id', $ya_id, PDO::PARAM_INT);
-                    $stmt->execute();
-                }
-            }*/
-            $stmt = $dbconnect->db->query('SELECT * FROM Genre');
-            $stmt->execute();
-            echo "<header>";
-            while($data = $stmt->fetch(PDO::FETCH_NUM)){
-                echo "<input type='button' id=''G'.$data[0]' value='$data[1]'>";
-            }
-            echo "</header>";
-            $db = null;
-           }catch(Exception $e){
-                throw new OriginalException($e);
-           }
-
+   try{
+    require_once "connect.php";
+    include 'error.php';
+    $dbconnect = new connect();
+    $stmt = $dbconnect->db->query('SELECT * FROM Genre');
+    $stmt->execute();
+    while($data = $stmt->fetch(PDO::FETCH_NUM)){
+        echo "<a href='Genre.php?Gid=$data[0]'>";
+        echo "<div class='items'>";
+        echo "<img src='串鳥メニュー画像/".$data[2]."'>";
+        echo "</div>";
+        echo "</a>";
+    }
+    echo "</div>";
+    echo "<div class=contentsA>";
+    if(isset($_GET["Gid"])){
+        $id = $_GET['Gid'];
+        $stmt = $dbconnect->db->prepare('SELECT * FROM Menu where Gid=:id');
+        $stmt->bindValue( ':id', $id, PDO::PARAM_INT);
+        $stmt->execute();
+        while($data = $stmt->fetch(PDO::FETCH_NUM)){
+            echo "<a href='Menu.php?Mid=$data[0]'>";
+            echo "<div class='menu'>";
+            echo "<p>$data[2]</p>";
+            echo "</div>";
+            echo "</a>";
+        }
+    }
+    $db = null;
+   }catch(Exception $e){
+        throw new OriginalException($e);
+   }
+   ?>
+    </div>
+    <div>
+        <div>
+            <input type="button" id="cartOrder" value="カート発注">
+        </div>
+        <div>
+            <tr><th>商品名</th><th>個数</th><th></th></tr>
+    <?php
+    /*if(isset($_GET['Mid']))
+    try{
+        require_once "connect.php";
+        include 'error.php';
+        $dbconnect = new connect();
+        $stmt = $dbconnect->db->query('SELECT * FROM Genre');
+        $stmt->execute();
+        while($data = $stmt->fetch(PDO::FETCH_NUM)){
+            //echo "<a href='Menu.php?id=$data[0]'>";
+            echo "<div class='items'>";
+            echo "<p>$data[1]</p>";
+            echo "<img src='img/".$data[2]."'>";
+            echo "</div>";
+            //echo "</a>";
+        }
+        $db = null;
+       }catch(Exception $e){
+            throw new OriginalException($e);
+       }   */ 
     ?>
+        </div>
+    </div>
     <footer>
-        <input type="button" id="kaikei" value="会計">
-        <input type="button" id="call" value="呼び出し">
-        <input type="button" id="Amenu" value="前のメニューへ">
-        <input type="button" id="Bmenu" value="次のメニューへ">
+        <input type="submit" id="kaikei" value="会計">
+        <input type="submit" id="call" value="呼び出し">
+        <input type="submit" id="login" value="ログイン">
     </footer>
 </body>
 </html>

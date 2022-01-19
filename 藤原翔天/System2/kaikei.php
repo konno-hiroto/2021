a25790a58b213d2f2197e5c4cd5478764cca101c
S-JOBI21-111\藤原 <211149@st.yoshida-g.ac.jp>
2022-01-19 11:14:36 +0900

    no message

--- 藤原翔天/System2/kaikei.php
+++ 藤原翔天/System2/kaikei.php
@@ -1,8 +1,8 @@
 <?php
     session_start();
-    /*$cartx = array();
+    $cartx = array();
     $pricex = array();
-    $good = array();*/
+    $good = array();
     //セッションcartxがある場合$cartxに
     if (isset($_SESSION['cartx'])) {
         $cartx = explode("/",$_SESSION['cartx']);
@@ -14,6 +14,50 @@
     //セッションgoodがある場合$goodに
     if(isset($_SESSION['goods'])){
         $goods = explode("/",$_SESSION['goods']);
+    }
+    //セッションSHがある場合$SHに
+    if(isset($_SESSION['SH'])){
+        $SH = explode("/",$_SESSION['SH']);
+    }
+    //セッションstockがある場合$SHに
+    if(isset($_SESSION['stock'])){
+        $stock = explode("/",$_SESSION['stock']);
+    }
+    //セッションGnumがある場合$Gnumに
+    if(isset($_SESSION['Gnum'])){
+        $Gnum = explode("/",$_SESSION['Gnum']);
+    }
+    if(isset($_POST['seisan'])){
+        $db = new PDO('mysql:host=localhost;dbname=training;charset=utf8','root','admin');
+        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        try{
+            $count = count($cartx);
+            $today = date("Y-m-d");
+            for($i=0; $i<$count; $i++){
+                $sql = 'INSERT INTO OrderList(DATE, Mid,Kosuu,Total) VALUES (:DATE, :Mid,:Kosuu,:Total)';
+                $stmt = $db->prepare($sql);
+                $stmt->bindParam(':DATE', $today,PDO::PARAM_STR);
+                $stmt->bindValue(':Mid', $SH[$i], PDO::PARAM_STR);
+                $stmt->bindValue(':Kosuu', $cartx[$i], PDO::PARAM_INT);
+                $stmt->bindValue(':Total', $cartx[$i]*$pricex[$i], PDO::PARAM_INT);
+                $stmt->execute();
+                if($Gnum[$i] == 1){
+                    $M = $stock[$i] - ($cartx[$i]*2);
+                }else{
+                    $M = $stock[$i] - $cartx[$i];
+                }
+                $stmt = $db->prepare('UPDATE Menu set stock=:stock where Mid=:Mid');
+                $stmt->bindValue( ':Mid', $SH[$i], PDO::PARAM_INT);
+                $stmt->bindValue( ':stock', $M,PDO::PARAM_INT);  
+                $stmt->execute();
+            }
+            header("Location:delete2.php");
+            exit();
+        }catch(PDOException $e){
+            print ("データベースに接続できませんでした".$e->getMessage());
+        }catch(Exception $e){
+            print ("予期せぬエラーです".$e->getMessage());
+        }
     }
 ?>
 <!DOCTYPE html>
@@ -40,23 +84,6 @@
 </head>
 <body>
     <div class="main">
-    <?php
-    /*
-    $count = count($cartx);
-    $total = 0;
-    for($i=0; $i<$count; $i++){
-        $total = $total + $pricex[$i]*$cartx[$i];
-        if($i == 0){
-            echo $goods[$i]." ".$pricex[$i]."×".$cartx[$i]." ";
-        }else if($i%5 == 0){
-            echo $goods[$i]." ".$pricex[$i]."×".$cartx[$i]."<br>";
-        }else{
-            echo $goods[$i]." ".$pricex[$i]."×".$cartx[$i]." ";
-        } 
-    }
-    echo $total."円";
-    */
-    ?>
     <table border="1" solid;>
         <tr><th colspan="3" align="center"><p>注文一覧</p></th></tr>
         <tr><td>商品名</td><td>個数</td><td>値段</td></tr>
@@ -72,7 +99,7 @@
             echo "<tr class='last'><td>合計</td><td>$Tnum"."個</td><td>$total"."円</td></tr>";
         ?>
     </table>
-    <form>
+    <form action="" method="post">
         <input type="submit" value="戻る" name="back">
         <input type="submit" value="清算" name="seisan">
     </form>
